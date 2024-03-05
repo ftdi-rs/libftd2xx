fe12aff5b40bda71abb53cd1220013bd1f272a33
Alex Martens <alex@thinglab.org>
2024-03-04 18:10:52 -0800

    version: 0.32.3 -> 0.32.4

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@
 //!
 //! ```toml
 //! [dependencies.libftd2xx]
-//! version = "0.32.3"
+//! version = "0.32.4"
 //! # statically link the vendor library, defaults to dynamic if not set
 //! # this will make things "just work" on Linux and Windows
 //! features = ["static"]
